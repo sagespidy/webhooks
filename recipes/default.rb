0d7ccca9424f5976d1fe8efb66bc1a727cb5fd8a
root <shailender.birmaan2@gmail.co>
2018-07-11 17:04:21 +0530

    dokken

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,11 @@
 # Cookbook:: git
 # Recipe:: default
 #
+#
+
+apt_update 'daily' do
+  frequency 86499
+  action :periodic
+end
+package "git"
 # Copyright:: 2018, The Authors, All Rights Reserved.
